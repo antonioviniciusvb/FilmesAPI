57bb88a0cbddbefe7ca956e972eda2b4db5f5c7d
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-09-30 14:19:44 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+
+        [StringLength(50, ErrorMessage = "O gênero não pode passar de 50 caracteres.")]
+        public string Genero { get; set; }
+
+        [Range(1, 300, ErrorMessage = "Duração deve ser de 1 a 300 minutos.")]
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
